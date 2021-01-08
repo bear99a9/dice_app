07ebce49fd2ab440f0c14347c692c994b9f0224a
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-08 12:06:56 +0000

    wrote rspec test for user story 5 and wrote code to pass

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,8 +1,8 @@
 class Dice
 
-  def roll(number =1 )
+  def roll(number = 1 )
     arr = []
     number.times { arr << rand(1..6)}
-    number == 1 ? arr.join.to_i : arr
+    arr
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,13 +4,13 @@ describe Dice do
 
   it { is_expected.to respond_to :roll }
 
-  it 'gives a number between 1 and 6 when rolled' do
-    expect(subject.roll).to be_between(1, 6)
-  end
-
   it { is_expected.to respond_to(:roll).with(1).argument}
 
   it "gives the right number of results" do
     expect(subject.roll(10).size).to eq 10
+  end
+
+  it "all results given back are between 1 and 6" do
+    expect(subject.roll(10)).to all( be_between(1, 6) )
   end
 end
