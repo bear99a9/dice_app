a9ee2c0253555258d2de94516188be4fdbb9c37e
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-08 11:58:46 +0000

    refactored code to pass feature test

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,7 +1,8 @@
 class Dice
 
-  def roll
-    rand(1..6)
+  def roll(number =1 )
+    arr = []
+    number.times { arr << rand(1..6)}
+    number == 1 ? arr.join.to_i : arr
   end
-
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -7,5 +7,6 @@ describe Dice do
   it 'gives a number between 1 and 6 when rolled' do
     expect(subject.roll).to be_between(1, 6)
   end
-  
+
+  it { is_expected.to respond_to(:roll).with(1).argument}
 end
